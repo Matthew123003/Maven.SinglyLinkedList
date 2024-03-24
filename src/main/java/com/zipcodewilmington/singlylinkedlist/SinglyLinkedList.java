5dc582138f200a94fea301e1ee190a774b169b0e
Matt Kramer <matthew123003@gmail.com>
2024-03-24 14:08:42 -0400

    Started making progress

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,155 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Iterator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> implements Iterable {
+
+
+
+    public class Node {
+        T data;
+        Node next;
+
+        public Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int N;
+
+
+    public SinglyLinkedList() {
+        first = null;
+        last = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void add(T item) {
+        if (item == null) {throw new NullPointerException();}
+        if (!isEmpty()) {
+            Node previous = last;
+            last = new Node(item, null);
+            previous.next = last;
+        }
+        else {
+            last = new Node(item, null);
+            first = last;
+        }
+        N++;
+    }
+
+    public boolean remove(T item) {
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty. Nothing to remove.");
+        }
+        boolean result = false;
+        Node current = first;
+        Node previous = first;
+        try {
+            while (current.next != null || current == last) {
+                if (current.data.equals(item)) {
+                    if (N == 1) {
+                        first = null;
+                        last = null;
+                    }
+                    //remove 1st element
+                    else if (current.equals(first)) {
+                        first = first.next;
+                    }
+                    //remove last
+                    else if (current.equals(last)) {
+                        previous = last;
+                        last.next = null;
+                    }
+                    //look for element and remove it
+                    else {
+                        previous.next = current.next;
+                    }
+                    N--;
+                    result = true;
+                }
+                previous = current;
+                current = previous.next;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught");
+        }
+
+        return result;
+    }
+
+    public boolean contains(T item) {
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty. Nothing to check.");
+        }
+        boolean result = false;
+        Node current = first;
+        while (current.next != null || current == last) {
+            if (current.data.equals(item)) {
+                result = true;
+                break;
+            } else {
+                current = current.next;
+
+            }
+        }
+
+
+        return result;
+    }
+
+    public int find(T item) {
+        int result = -1;
+        Node current = first;
+        while (current.next != null || current == last) {
+            if (current.data.equals(item)) {
+                result++;
+                break;
+            } else {
+                current = current.next;
+
+            }
+            result++;
+        }
+        return result;
+
+    }
+
+    public T get(int index) {
+        T result = null;
+        Node current = first;
+        for(int i = 0; i <= N; i++){
+            if (i == index) {
+                result = current.data;
+            } else {
+                current = current.next;
+
+            };
+        }
+        return result;
+    }
+
+
+
+
+
+
+        @Override
+    public Iterator iterator() {
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,95 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
-}
+
+        @Test
+        public void linkedListAddTest() {
+            SinglyLinkedList list  = new SinglyLinkedList<>();
+
+            list.add(1);
+            list.add(2);
+            boolean assertListContains = list.isEmpty();
+            Assert.assertFalse(assertListContains);
+        }
+
+        @Test
+        public void linkedListRemoveTest() {
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.add(1);
+            list.add(2);
+            list.add("pizzaz");
+            list.add("hotdog");
+
+            int expectedListSize = 3;
+            list.remove("pizzaz");
+            int actualListSize = list.size();
+
+            Assert.assertEquals(expectedListSize, actualListSize);
+        }
+
+
+        @Test
+        public void linkedListContainsTest(){
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            //Object expectedItemInList = 3;
+            boolean actualItemInList = list.contains(3);
+
+            Assert.assertTrue(actualItemInList);
+        }
+
+        @Test
+         public void linkedListFindTest(){
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            int expectedIndex = 2;
+            int actualIndex = list.find(3);
+
+            Assert.assertEquals(expectedIndex, actualIndex);
+
+        }
+
+        @Test
+        public void linkedListGetTest(){
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            Object expectedItemReturned = 3;
+            Object actualItemReturned = list.get(2);
+
+            Assert.assertEquals(expectedItemReturned, actualItemReturned);
+        }
+
+        @Test
+        public void linkedListSizeTest(){
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            int expectedSize = 3;
+            int actualSize = list.size();
+
+            Assert.assertEquals(expectedSize, actualSize);
+        }
+
+        @Test
+        public void linkedListCopyTest(){
+
+        }
+
+    }
